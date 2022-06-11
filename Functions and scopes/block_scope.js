5b4a79d7bd59c9fad1582c76bd1531af193db613
Teo <teo.bostiog@yahoo.com>
2022-06-11 21:02:15 +0300

    block scope

---
+++ Functions and scopes/block_scope.js
@@ -0,0 +1,15 @@
+let message = 'Outside';
+if ( 5 === 5) {
+    let message = 'Equal';
+    console.log(message); // Equal
+}
+console.log(message); // Outside
+
+
+var message1 = 'Outside';
+if ( 5===5) {
+    var message1 = 'Equal';
+    console.log(message1); // Equal
+}
+console.log(message1); // Equal
+
