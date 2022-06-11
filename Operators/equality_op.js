7d9580768032e374415ddab28cd1286efcb4d86f
Teo <teo.bostiog@yahoo.com>
2022-06-11 17:44:18 +0300

    equality operators

---
+++ Operators/equality_op.js
@@ -0,0 +1,11 @@
+
+console.log( 1 == 1); // true
+console.log( 1 == true); // true
+console.log( 1 === 1); // false (one is a number, one is a boolean)
+
+let id = 123;
+console.log( id === "123"); //true 
+console.log( id != "123"); //false
+console.log( id !== "123"); //true 
+
+
