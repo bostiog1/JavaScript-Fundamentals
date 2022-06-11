4332f92a727dfb6a89f6e25d4665009245864fa7
Teo <teo.bostiog@yahoo.com>
2022-06-11 18:05:38 +0300

    assignment operators

---
+++ Operators/assignment_op.js
@@ -0,0 +1,22 @@
+let year = 1964;
+year += 10;
+console.log(year); //1974
+
+let year1 = 1964;
+year -= 10;
+console.log(year1); //1954
+
+
+let year2 = 1964;
+year >>= 1;
+console.log(year2); 
+//982  
+//equal to year2 divided by 2, 1964/2
+
+let year3 = 1964;
+year <<= 1;
+console.log(year3); 
+//3928
+//equal to year2 multiply by 2, 1964*2
+
+
