afa396d8e45bdbd5c77a6170e7b01879616683d2
Teo <teo.bostiog@yahoo.com>
2022-06-12 02:50:09 +0300

    com

---
+++ 6. BOM and DOM/dom.js
@@ -0,0 +1,5 @@
+let element = document.getElementById('elementId');
+element.textContent = 'new text here';
+element.setAttribute('name', 'nameValue');
+element.classList.add('myClassName');
+element.style.color = 'blue';
