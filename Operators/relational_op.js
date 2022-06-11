5e2056333dec367eff77d520c70e41239b455be5
Teo <teo.bostiog@yahoo.com>
2022-06-11 18:00:51 +0300

    relational operators

---
+++ Operators/relational_op.js
@@ -0,0 +1,12 @@
+let s1 = 'Zoo';
+let s2 = 'alphabet';
+
+if(se1 < s2) {
+    console.log(true);
+}
+else {
+    console.log(false);
+}
+//true bc uppercase letter 
+//come before lowercase letter 
+// ASCII
