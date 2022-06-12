7e43a61ced1f180c47a1fa74f035dcf4b2a105d7
Teo <teo.bostiog@yahoo.com>
2022-06-13 00:50:52 +0300

    setting a promise

---
+++ 7. Promises and Error Handling/setting_a_promise.js
@@ -0,0 +1,13 @@
+let promise = new Promise(
+    function(resolve, reject) {
+        setTimeout(resolve, 100, 'someValue');
+    }
+);
+
+promise.then(
+    value => console.log('fulfilled: ' + value),
+    error => console.log('rejected: ' + error)
+);
+
+
+
