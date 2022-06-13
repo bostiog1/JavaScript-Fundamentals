69270de6243ef0b0df15789e210cc084f46ef6c3
Teo <teo.bostiog@yahoo.com>
2022-06-14 00:52:49 +0300

    SSL

---
+++ 9. Security and Production/ssl.js
@@ -0,0 +1,10 @@
+// use SSL on the server side or http side
+// with SSL communications are encrypted making " the man in the middle" attacks much less likely
+
+
+//use HTTP header: strict-transport-security
+
+//use cookie attributed: secure and HttpOnly
+
+
+
